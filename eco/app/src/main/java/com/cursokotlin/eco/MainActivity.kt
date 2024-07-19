71ba1e5a13e3f23370c327300cf6fd8e8bafbc65
Sp00kyDucklyn <hoshi.saga.dokuringo@gmail.com>
2024-07-19 09:39:26 -0700

    feat: vistas mejoradas
    cambiaron vistas, home, etiquetas, project  y album

--- eco/app/src/main/java/com/cursokotlin/eco/MainActivity.kt
+++ eco/app/src/main/java/com/cursokotlin/eco/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.cursokotlin.eco.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNav.background=null
+        val addsBtn: FloatingActionButton
+
+
+        bottomNav.setupWithNavController(navController)
+
+
+        // Configurar el listener para manejar la navegación cuando se hace clic en un elemento del menú
+        bottomNav.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_Home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.menu_Etiquete -> {
+                    navController.navigate(R.id.etiquetesFragment)
+                    true
+                }
+                else -> false
+
+            }
         }
     }
 }
